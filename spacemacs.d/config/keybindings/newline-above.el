bed73c1a3474d06dcdf82a79c16b058e32e6ee7e
Rolf Håvard Blindheim <rhblind@gmail.com>
2020-09-17 13:18:38 +0200

    spacemacs config moved to their own repo

--- spacemacs.d/config/keybindings/newline-above.el
+++
@@ -1,8 +0,0 @@
-(defun newline-above()
-  "Inserts a new line above the current line"
-  (interactive)
-  (let ((oldpos (point)))
-    (beginning-of-line)
-    (open-line 1)
-    (indent-according-to-mode)))
-(provide 'newline-above)
--- spacemacs.d/config/keybindings/newline-below.el
+++
@@ -1,7 +0,0 @@
-(defun newline-below ()
-  "Insert an empty line below the current line."
-  (interactive)
-  (let ((oldpos (point)))
-    (end-of-line)
-    (newline-and-indent)))
-(provide 'newline-below)
